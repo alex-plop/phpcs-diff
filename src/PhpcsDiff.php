1da8cdacf5d073987d1b7cdbf66baaa54536a149
Alex Plop <aplop@awesomemotive.com>
2023-01-17 13:49:04 +0200

    Fix to avoid fatal errors from vsprint in wpcs package

--- src/PhpcsDiff.php
+++ src/PhpcsDiff.php
@@ -218,7 +218,7 @@ class PhpcsDiff
         }
 
         $exec = PHP_BINARY . ' ' . $exec;
-        $command = $exec . ' --report=json --standard=' . $ruleset . ' ' . implode(' ', $files);
+        $command = $exec . ' --report=json --standard=' . $ruleset . ' **/*.php';
         $output = shell_exec($command);
 
         // Filter out non-json output.
